c4f4372657b354e4672d3b78d3a97bc288e3ed0b
andros lee <eighdrose@gmail.com>
2022-10-18 20:54:31 -0600

    initial commit, much ado about nothing

---
+++ parser.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+
+'''
+
+lets build a text parser that reads in a raw symphony payload, and can export it to whatever endpoint we like
+human readable output
+quantconnect
+vectorbt
+tradingview
+thinkscript
+
+
+'''
