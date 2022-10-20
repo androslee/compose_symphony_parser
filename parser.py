345abee171c6d1f6478e038c4bf297fba04c1afe
andros lee <eighdrose@gmail.com>
2022-10-19 19:49:00 -0600

    initial human readable output added to main class!

--- parser.py
+++ parser.py
@@ -12,7 +12,9 @@ thinkscript
 
 
 '''
+import edn_format
 import argparse
+import json
 import sys
 
 
@@ -21,13 +23,17 @@ class InFileReader:
     
     def __init__(self, filePath):
         self.filePath = filePath
+        self.data = None
         return
         
         
     def readFile(self):
-        with open(self.filePath, "rb") as infile:
-            for line in infile:
-                print(line)
+        with open(self.filePath, "r") as infile:
+            self.data = infile.read()
+        #  'inputFile.edn'
+        #data_with_wrapping_string_removed = json.load(open(self.filePath, 'r'))
+        #self.data = edn_format.loads(data_with_wrapping_string_removed)
+
         return
         
         
@@ -45,11 +51,44 @@ class OutfileBase:
 #TODO finish    
 class OutfileHuman(OutfileBase):
     
-    
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, filePath):
+        super().__init__()
+        self.filePath = filePath
         return
+    
+    def show(self, data):
+        data = data.replace(', :', ',:')
+        data = data.split(",")
+        x = 0
+        y = 0
+        tabcount = 0
+        tabstring = ""
+        parsestring = ""
+
+        while x < len(data):
+
+            if "[" in data[x]:
+                count = data[x].count("[")
+                y = y + count
+            if "]" in data[x - 1]:
+                count = data[x].count("]")
+                y = y - count
+
+            tabstring = "\n"
+            while tabcount < y:
+                tabstring = tabstring + "  "
+                tabcount = tabcount + 1
+
+            tabcount = 0
+
+            data[x] = tabstring + data[x] + ","
+
+            parsestring = parsestring + data[x]
+
+            x = x + 1
         
+        print(parsestring)
+        return
         
     
     
@@ -77,6 +116,9 @@ def main()-> int:
     inFileParser = InFileReader(args["infile"])
     inFileParser.readFile()
     
+    humanParser = OutfileHuman(args["infile"])
+    
+    humanParser.show(inFileParser.data)
     
     return 0
 #TODO make generic class for output mode
