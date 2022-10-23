33bb4e2b7b5b3915d857a1d11c818f3d87c5003c
andros lee <eighdrose@gmail.com>
2022-10-22 23:05:40 -0600

    added bulk reading of url links from file.  added sample bulk url file

--- parser.py
+++ parser.py
@@ -45,21 +45,31 @@ class InFileReader:
             owner = "UNKNOWN"
             created = "UNKNOWN"
             name = "UNKNOWN"
+            description = "UNKNOWN"
+            symph_name = "UNKNOWN"
+            
+            
             if "owner" in self.resp:
                 owner = self.resp['owner']
             if "created" in self.resp:
                 owner = self.resp['created']
             if "name" in self.resp:
                 owner = self.resp['name']
+            if "description" in self.resp:
+                description = self.resp['description']
+            if self.root_node:
+                symph_name = self.root_node["name"]
             
             
             print("""\r\n========================================================\r\n
-            owner: %s\r\n
-            name:  %s\r\n
-            created at:%s \r\n
+            owner:......... %s\r\n
+            creator_name:.. %s\r\n
+            created at:.... %s \r\n
+            symphony name:. %s\r\n
+            description at: %s \r\n
                   
                   
-                  """ % (owner, name, created ))
+                  """ % (owner, name, created, symph_name, description ))
             
             
     def readFile(self, url_loaded = False):
@@ -201,7 +211,7 @@ def main()-> int:
     parser.add_argument('-i','--infile', dest="infile", action="store", help=' input file we read the symphony text from.  full path please', required=True)
     parser.add_argument('-o','--outfile', dest="outfile", action="store", default="OUTFILE", help=' output file to save the parsed text to.  if not given, will use stdout', required=False)
     parser.add_argument('-m','--mode', dest="mode", action="store", default="human", help=' output parsing mode to use.  if none given, will parse for "human readable output".  modes are: quantconnect, vectorbt, tradingview, thinkscript', required=False)
-    
+    parser.add_argument('-b', '--bulk', action="store_true", dest='bulk', default='False', help="it means the specified input is a filepath, containing a bulk list of urls, or filenames to process.  one url or file path per line")
     
     parser.add_argument('-u', '--url', action="store_true", dest='url', default='False', help="specifies that the input file path is actually the url to a shared, public symphony on composer.trade")
     parser.add_argument('-p', '--parent', action="store_true", dest='parent', default='False', help="specifies that we should try and look up the parents of this symphony, and get all previous copied information too.  only works if the 'infile' given was a url")
@@ -210,77 +220,111 @@ def main()-> int:
     args = vars(parser.parse_args())
 
     if args['url'] == True:
+        url_list = []
+        if args['bulk'] == True:
+            # read bulk file and add them all to the "list"
+            with open(args['infile'], 'r') as bulkfile:
+                for line in bulkfile.readlines():
+                    url_list.append(line)
+        else:
+            url_list.append(args['infile'])
+            # just add the single one to the list and have it process the "list" anyways
+        
+        
+        
+        
         #todo move to request this from the composer site instead of hardcoding
         composerConfig = {
                 "projectId" : "leverheads-278521",
                 "databaseName" : "(default)"
             }
-        m = re.search('\/symphony\/([^\/]+)', args["infile"])
-        symphId = m.groups(1)[0]
-        
-        current_symph_id = symphId
-        response_list = []
-        while current_symph_id:
-            symphReq = requests.get(f'https://firestore.googleapis.com/v1/projects/{composerConfig["projectId"]}/databases/{composerConfig["databaseName"]}/documents/symphony/{symphId}')
-            resp = json.loads(symphReq.text)
-            # 'latest_backtest_info', 'latest_backtest_edn', 'latest_version', 'hashtag', 'owner', 'description', 'created_at', 'latest_version_edn', 'sparkgraph_url', 'color', 'name', 'share-with-everyone?', 'stats', 'last_updated_at', 'youtube-url', 'cached_rebalance', 'latest_backtest_run_at', 'cached_rebalance_corridor_width', 'copied-from', 'backtest_url'])
-
-            if 'fields' not in resp:
-                print("\r\nWas this a private symphony link? response 'object' had no 'fields' key.  could not parse\r\n  Error 2")
-                #sys.exit(2)
-                current_symph_id = None
-                
-            if args['parent'] == True:
-                if 'copied-from' in resp['fields']:
-                    print("old id \r\n %s \r\nnew id \r\n %s\r\n" % (current_symph_id, resp['fields']['copied-from']['stringValue']))
-                    if current_symph_id == resp['fields']['copied-from']['stringValue']:
-                        print("copied from fields are the same, no more parents, ending the lookups")
-                        # set current id to None, which should end our loop, and let us start looping over all our responses
-                        current_symph_id = None
-                    else:
-                        # different parent id found, copy it, and then lets loop over and grab the next one
-                        current_symph_id = resp['fields']['copied-from']['stringValue']
-                else:
-                    current_symph_id = None
-                sleep_time = 2 + 20 * random.random()
-                print("-->random delay of %s between requests" % str(sleep_time))
-                time.sleep(sleep_time)
-                
-            # user did not ask for a "symphony parent lookup, so we will not try to loop over things
-            else:
-                print("---> skipping symphony parent check")
-                current_symph_id = None
-            # whatever the response was, copy it into our master response list.  we'll use all the responses later
-            response_list.append(copy.deepcopy(resp))
-        #import pdb; pdb.set_trace()
-        
-        for resp in response_list:
-            print(json.dumps(resp['fields']['latest_version_edn']['stringValue'], indent=2))
-            inFileParser = InFileReader(None, resp)
-            inFileParser.printHeader(url_loaded = True)
-            inFileParser.readFile(url_loaded = True)
+        total = len(url_list)
+        for count, current_url in enumerate(url_list):
             
+            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+            print("****************************************************************************************************************")
+            print("                   starting a new bulk lookup                   %s of %s        " % (str(count), str(total)))
+            print("****************************************************************************************************************")
+            print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+            
+            m = re.search('\/symphony\/([^\/]+)', current_url)
+            symphId = m.groups(1)[0]
+
+            current_symph_id = symphId
+            response_list = []
+            while current_symph_id:
+                symphReq = requests.get(f'https://firestore.googleapis.com/v1/projects/{composerConfig["projectId"]}/databases/{composerConfig["databaseName"]}/documents/symphony/{symphId}')
+                resp = json.loads(symphReq.text)
+                # 'latest_backtest_info', 'latest_backtest_edn', 'latest_version', 'hashtag', 'owner', 'description', 'created_at', 'latest_version_edn', 'sparkgraph_url', 'color', 'name', 'share-with-everyone?', 'stats', 'last_updated_at', 'youtube-url', 'cached_rebalance', 'latest_backtest_run_at', 'cached_rebalance_corridor_width', 'copied-from', 'backtest_url'])
+
+                if 'fields' not in resp:
+                    print("\r\nWas this a private symphony link? response 'object' had no 'fields' key.  could not parse\r\n  Error 2")
+                    #sys.exit(2)
+                    current_symph_id = None
+
+                if args['parent'] == True:
+                    if 'copied-from' in resp['fields']:
+                        print("old id \r\n %s \r\nnew id \r\n %s\r\n" % (current_symph_id, resp['fields']['copied-from']['stringValue']))
+                        if current_symph_id == resp['fields']['copied-from']['stringValue']:
+                            print("copied from fields are the same, no more parents, ending the lookups")
+                            # set current id to None, which should end our loop, and let us start looping over all our responses
+                            current_symph_id = None
+                        else:
+                            # different parent id found, copy it, and then lets loop over and grab the next one
+                            current_symph_id = resp['fields']['copied-from']['stringValue']
+                    else:
+                        current_symph_id = None
+                    sleep_time = 2 + 20 * random.random()
+                    print("-->random delay of %s between requests" % str(sleep_time))
+                    time.sleep(sleep_time)
+
+                # user did not ask for a "symphony parent lookup, so we will not try to loop over things
+                else:
+                    print("---> skipping symphony parent check")
+                    current_symph_id = None
+                # whatever the response was, copy it into our master response list.  we'll use all the responses later
+                response_list.append(copy.deepcopy(resp))
+            #import pdb; pdb.set_trace()
+
+            for resp in response_list:
+                print(json.dumps(resp['fields']['latest_version_edn']['stringValue'], indent=2))
+                inFileParser = InFileReader(None, resp)
+                inFileParser.printHeader(url_loaded = True)
+                inFileParser.readFile(url_loaded = True)
+
+                if args["mode"] == "human":
+                    humanParser = OutfileHuman(args["infile"])
+                    humanParser.show(inFileParser.root_node)
+
+                if args["mode"] == "vector":
+                    vectorParser = OutfileVectorBt()
+                    vectorParser.show(inFileParser.data)
+
+    else:
+        file_list = []
+        if args['bulk'] == True:
+            # read bulk file and add them all to the "list"
+            with open(args['infile'], 'r') as bulkfile:
+                for line in bulkfile.readlines():
+                    file_list.append(line)
+        else:
+            file_list.append(args['infile'])
+            # just add the single one to the list and have it process the "list" anyways
+            
+            
+        for file in file_list:
+            print(file)
+            inFileParser = InFileReader(file, None)
+            inFileParser.readFile()
+
+
             if args["mode"] == "human":
-                humanParser = OutfileHuman(args["infile"])
+                humanParser = OutfileHuman(file)
                 humanParser.show(inFileParser.root_node)
 
             if args["mode"] == "vector":
                 vectorParser = OutfileVectorBt()
                 vectorParser.show(inFileParser.data)
-            
-    else:
-        print(args["infile"])
-        inFileParser = InFileReader(args["infile"], None)
-        inFileParser.readFile()
-
-    
-        if args["mode"] == "human":
-            humanParser = OutfileHuman(args["infile"])
-            humanParser.show(inFileParser.root_node)
-
-        if args["mode"] == "vector":
-            vectorParser = OutfileVectorBt()
-            vectorParser.show(inFileParser.data)
     
     return 0
 #TODO make generic class for output mode
