ce660c4de9745bce11b39f02245aa576399b0b73
James Fulford <james.fulford@outlook.com>
2022-10-25 16:38:11 -0400

    fix(edn_syntax): case where keys are dictionaries -> tuples

--- lib/edn_syntax.py
+++ lib/edn_syntax.py
@@ -1,12 +1,27 @@
+import pprint
 import edn_format
+
+from . import manual_testing
+
+
+def convert_edn_to_immutable_value(d):
+    if type(d) == edn_format.immutable_dict.ImmutableDict:
+        return tuple([(convert_edn_to_immutable_value(k), convert_edn_to_immutable_value(v))
+                      for k, v in d.items()])
+    else:
+        return convert_edn_to_pythonic(d)
 
 
 def convert_edn_to_pythonic(d):
     if type(d) == edn_format.immutable_dict.ImmutableDict:
-        return {convert_edn_to_pythonic(k): convert_edn_to_pythonic(v) for k, v in d.items()}
+        return {convert_edn_to_immutable_value(k): convert_edn_to_pythonic(v) for k, v in d.items()}
     elif type(d) == edn_format.immutable_list.ImmutableList:
         return [convert_edn_to_pythonic(v) for v in d]
     elif type(d) == edn_format.edn_lex.Keyword:
         return ":" + d.name
     else:
         return d
+
+
+def main():
+    pprint.pprint(manual_testing.get_root_node_from_path("inputs/weird.edn"))
